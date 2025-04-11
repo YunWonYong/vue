cd86bba7017387ae1afc355c05632f4af9a7f7c1
wonyong.yun <ywyi1992@gmail.com>
2025-04-12 00:36:17 +0900

    crash vue one day clear

--- crash-vue3_25_04_11/src/App.vue
+++ crash-vue3_25_04_11/src/App.vue
@@ -1,5 +1,14 @@
+<script setup>
+    import Navbar from "@/components/Navbar.vue";
+    import Hero from "@/components/Hero.vue";
+    import HomeCards from "@/components/HomeCards.vue";
+    import Joblistings from "@/components/JobListings.vue";
+</script>
 <template>
-    <h1 class="text-2xl">
-        Vue Jobs
-    </h1>
+    <Navbar />
+    <Hero
+        title="Test Title"
+    />
+    <HomeCards />
+    <Joblistings />
 </template>
