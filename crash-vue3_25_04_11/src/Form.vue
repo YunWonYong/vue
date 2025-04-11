b7577fbf7eda75ef6502e7d50b3ba4d310aea90a
wonyong.yun <ywyi1992@gmail.com>
2025-04-11 23:27:17 +0900

    crash vue From

---
+++ crash-vue3_25_04_11/src/Form.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
+const newTask = ref("");
+const toggleStatus = () => {
+    if (status.value === "active") {
+        status.value = "pending";
+    } else if (status.value === "pending") {
+        status.value = "inactive";
+    } else {
+        status.value = "active";
+    }
+};
+
+const addTask = () => {
+    if (newTask.value.trim() !== "") {
+        tasks.value.push(newTask.value);
+        newTask.value = "";
+    }
+};
+
+const deleteTake = (index) => {
+    tasks.value.splice(index, 1);
+};
+</script>
+<template>
+    <form @submit.prevent="addTask">
+        <label for="newTask">
+            Add Task
+        </label>
+        <input type="text" id="newTask" name="newTask" v-model="newTask"/>
+        <button type="submit">
+            Submit
+        </button>
+    </form>
+    <ul>
+        <li v-for="(task, index) in tasks" :key="tash">
+            <span>
+                {{ task }}
+            </span>
+            <button @click="deleteTake(index)">x</button>
+        </li>
+    </ul>
+</template>
