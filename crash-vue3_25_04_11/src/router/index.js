a1b0a9ee1c46fa7b410814160279e38923912e58
wonyong.yun <ywyi1992@gmail.com>
2025-04-16 22:51:42 +0900

    crash vue jobsView and notFoundView

--- crash-vue3_25_04_11/src/router/index.js
+++ crash-vue3_25_04_11/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-
+import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -8,6 +9,16 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeView,
+        },
+        {
+            path: "/jobs",
+            name: "jobs",
+            component: JobsView,
+        },
+        {
+            path: "/:catchAll(.*)",
+            name: "not-found",
+            component: NotFoundView,
         }
     ],
 });
