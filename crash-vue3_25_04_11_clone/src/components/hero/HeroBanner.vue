4082b9911bb722f49112b5f658e0c4825474d840
wonyong.yun <ywyi1992@gmail.com>
2025-04-26 01:26:07 +0900

    Add Hero Banner

---
+++ crash-vue3_25_04_11_clone/src/components/hero/HeroBanner.vue
@@ -0,0 +1,44 @@
+<script setup="ts">
+import { defineProps } from "vue";
+defineProps({
+    title: {
+        type: String,
+        default: "Become a Vue Dev"
+    },
+    subtitle: {
+        type: String,
+        default: "Find the Vue job that fits your skills and needs",
+    }
+});
+
+</script>
+<template>
+<section
+    :class="$style.hero"
+>
+    <article
+        :class="$style.heroInner"
+    >
+        <h1>
+            {{ title }}
+        </h1>
+        <p>
+            {{ subtitle }}
+        </p>
+    </article>
+</section>
+</template>
+<style module>
+.hero {
+    background-color: rgb(21, 128, 61, 1);
+    margin-bottom: 1rem;
+    padding:5rem 0;
+}
+
+.hero-inner {
+    max-width: 80rem;
+    margin: 0 auto;
+    padding: 0 2rem;
+    display: flex;
+}
+</style>
--- crash-vue3_25_04_11_clone/src/components/layout/MainHeader.vue
+++ crash-vue3_25_04_11_clone/src/components/layout/MainHeader.vue
@@ -1,8 +1,10 @@
 <script setup>
+import HeroBanner from '../hero/HeroBanner.vue';
 import MainNavigation from './MainNavigation.vue';
 </script>
 <template>
 <header>
     <MainNavigation />
+    <HeroBanner />
 </header>
 </template>
